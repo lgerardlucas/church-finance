7641f13a4dc167d3683a4b980b11c3d287871c71
lgerardlucas <lgerarddlucas@gmail.com>
2023-08-29 22:20:38 -0300

    Validação dos campos antes de persistir.

--- src/controllers/churchController.js
+++ src/controllers/churchController.js
@@ -1,4 +1,5 @@
 // Lida com as requisições para o banco de dados
+const { check, validationResult } = require("express-validator");
 const {
   createChurch,
   listChurch,
@@ -8,6 +9,12 @@ const {
 } = require("../services/churchService");
 
 exports.createChurch = async (req, res) => {
+  // Verifica os erros de validação
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   try {
     const church = await createChurch(req.body);
     res.status(200).json(church);
@@ -44,6 +51,12 @@ exports.deleteChurch = async (req, res) => {
 };
 
 exports.updateChurch = async (req, res) => {
+  // Verifica os erros de validação
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   try {
     const churchs = await updateChurch(req.params.id, req.body);
     res.status(200).json(churchs);
--- src/routes/churchRoutes.js
+++ src/routes/churchRoutes.js
@@ -1,5 +1,6 @@
 // Recebe as requisições para o banco de dados
 const express = require("express");
+const { check } = require("express-validator");
 const {
   createChurch,
   listChurch,
@@ -10,12 +11,83 @@ const {
 
 const router = express.Router();
 
-router.post("/church", createChurch);
+// Validar campos de uma requisição
+const validateChurchFields = [
+  check("name")
+    .optional({ checkFalsy: true })
+    .isLength({ min: 5 })
+    .withMessage("Mínimo de 5 caracteres.")
+    .custom((value, { req }) => {
+      if (!value && req.method === "PATCH") {
+        throw new Error("O campo não pode estar vazio");
+      }
+      return true;
+    }),
+
+  check("address")
+    .optional({ checkFalsy: true })
+    .isLength({ min: 5 })
+    .withMessage("Mínimo de 5 caracteres.")
+    .custom((value, { req }) => {
+      if (!value && req.method === "PATCH") {
+        throw new Error("O campo não pode estar vazio");
+      }
+      return true;
+    }),
+
+  check("city")
+    .optional({ checkFalsy: true })
+    .isLength({ min: 5 })
+    .withMessage("Mínimo de 5 caracteres.")
+    .custom((value, { req }) => {
+      if (!value && req.method === "PATCH") {
+        throw new Error("O campo não pode estar vazio");
+      }
+      return true;
+    }),
+
+  check("state")
+    .optional({ checkFalsy: true })
+    .isLength({ min: 5 })
+    .withMessage("Mínimo de 5 caracteres.")
+    .custom((value, { req }) => {
+      if (!value && req.method === "PATCH") {
+        throw new Error("O campo não pode estar vazio");
+      }
+      return true;
+    }),
+
+  check("country")
+    .optional({ checkFalsy: true })
+    .isLength({ min: 5 })
+    .withMessage("Mínimo de 5 caracteres.")
+    .custom((value, { req }) => {
+      if (!value && req.method === "PATCH") {
+        throw new Error("O campo não pode estar vazio");
+      }
+      return true;
+    }),
+
+  check("zipcode")
+    .optional({ checkFalsy: true })
+    .isNumeric()
+    .withMessage("Campo deve conter somente números")
+    .isLength({ min: 8, max: 8 })
+    .withMessage("Campo deve ter 8 dígitos.")
+    .custom((value, { req }) => {
+      if (!value && req.method === "PATCH") {
+        throw new Error("O campo não pode estar vazio");
+      }
+      return true;
+    }),
+];
+
+router.post("/church", validateChurchFields, createChurch);
 
 router.get("/churchs", listChurch).get("/church/:id", findChurch);
 
 router.delete("/church/:id", deleteChurch);
 
-router.patch("/church/:id", updateChurch);
+router.patch("/church/:id", validateChurchFields, updateChurch);
 
 module.exports = router;
